407e1905fdda8c7d45123998512f404e213a788f
David Kant <david.kant@gmail.com>
2020-06-16 11:28:28 -0700

    Add temporal centroid tests

--- test/trending_test.py
+++ test/trending_test.py
@@ -48,6 +48,47 @@ def target_spec_dunn_win_600():
     print('OK')
     return 1
 
+def test_temporal_centroid():
+    alpha = np.load('test/data/trending_dunn_win_600_alpha_200608.npy')
+    bins = np.load('test/data/trending_dunn_win_600_bins_200608.npy')
+    centroid = tr.temporal_centroid(alpha, bins)
+    assert centroid == 7.847722156318984
+    print('OK')
+    return 1
+
+def test_temporal_centroid_custom_spec():
+    alpha = np.load('test/data/trending_dunn_win_600_alpha_200608.npy')
+    bins = np.load('test/data/trending_dunn_win_600_bins_200608.npy')
+    target_spec = [
+        np.inf,
+        600,
+        300,
+        200,
+        150,
+        120,
+        100,
+        85,
+        70,
+        60,
+        50,
+        40,
+        30,
+        20,
+        15,
+        10,
+        5,
+        4,
+        3,
+        2,
+        1,
+        0,
+    ]
+    new_spec = tr.FFT_to_target_spec(alpha, bins, target_spec, aggr_func=np.max)
+    new_spec_test = np.load('test/data/target_spec_dunn_win_600_200608.npy')
+    centroid = tr.temporal_centroid(new_spec, target_spec)
+    assert centroid == 113.70549064467077
+    print('OK')
+    return 1
 
 trending_dunn_win_600()
 target_spec_dunn_win_600()
